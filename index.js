570556fa904099f8704ee088183e9d058fb1b75f
MuhammadSaadMubeen <94379462+MuhammadSaadMubeen@users.noreply.github.com>
2021-11-16 11:33:25 +0500

    First Version

---
+++ index.js
@@ -0,0 +1,162 @@
+
+
+function home() {
+    document.getElementById("home").style.background = "rgb(18, 77, 18)";
+    document.getElementById("home").style.color = "White";
+}
+
+function home1() {
+    document.getElementById("home").style.background = "rgb(97, 97, 255)";
+    document.getElementById("home").style.color = "rgba(255,255,255,.55)";
+}
+
+function voice() {
+    document.getElementById("voice").style.background = "rgb(18, 77, 18)";
+    document.getElementById("voice").style.color = "white";
+}
+
+function voice1() {
+    document.getElementById("voice").style.background = "rgb(97, 97, 255)";
+    document.getElementById("voice").style.color = "rgba(255,255,255,.55)";
+}
+
+function login() {
+    document.getElementById("login").style.background = "rgb(18, 77, 18)";
+    document.getElementById("login").style.color = "white";
+}
+
+function login1() {
+    document.getElementById("login").style.background = "rgb(97, 97, 255)";
+    document.getElementById("login").style.color = "rgba(255,255,255,.55)";
+}
+
+
+
+function invoice() {
+    document.getElementById("invoice").style.background = "rgb(18, 77, 18)";
+    document.getElementById("invoice").style.color = "white";
+}
+
+function invoice1() {
+    document.getElementById("invoice").style.background = "white";
+    document.getElementById("invoice").style.color = "black";
+}
+
+function previous() {
+    document.getElementById("previous").style.background = "rgb(18, 77, 18)";
+    document.getElementById("previous").style.color = "white";
+}
+
+function previous1() {
+    document.getElementById("previous").style.background = "white";
+    document.getElementById("previous").style.color = "black";
+}
+
+function refund() {
+    document.getElementById("refund").style.background = "rgb(18, 77, 18)";
+    document.getElementById("refund").style.color = "white";
+}
+
+function refund1() {
+    document.getElementById("refund").style.background = "white";
+    document.getElementById("refund").style.color = "black";
+}
+
+function exchange() {
+    document.getElementById("exchange").style.background = "rgb(18, 77, 18)";
+    document.getElementById("exchange").style.color = "white";
+}
+
+function exchange1() {
+    document.getElementById("exchange").style.background = "white";
+    document.getElementById("exchange").style.color = "black";
+}
+function user() {
+    document.getElementById("user").style.background = "#4b56f3";
+    document.getElementById("user").style.color = "white";
+}
+
+function user1() {
+    document.getElementById("user").style.background = "#fff";
+    document.getElementById("user").style.color = "black";
+}
+
+
+function staff() {
+    document.getElementById("staff").style.background = "#4b56f3";
+    document.getElementById("staff").style.color = "white";
+}
+
+function staff1() {
+    document.getElementById("staff").style.background = "#fff";
+    document.getElementById("staff").style.color = "black";
+}
+function purchase() {
+    document.getElementById("purchase").style.background = "#4b56f3";
+    document.getElementById("purchase").style.color = "white";
+}
+
+function purchase1() {
+    document.getElementById("purchase").style.background = "#fff";
+    document.getElementById("purchase").style.color = "black";
+}
+function account() {
+    document.getElementById("account").style.background = "#4b56f3";
+    document.getElementById("account").style.color = "white";
+}
+
+function account1() {
+    document.getElementById("account").style.background = "#fff";
+    document.getElementById("account").style.color = "black";
+}
+
+function sale() {
+    document.getElementById("sale").style.background = "#4b56f3";
+    document.getElementById("sale").style.color = "white";
+}
+
+function sale1() {
+    document.getElementById("sale").style.background = "#fff";
+    document.getElementById("sale").style.color = "black";
+}
+
+function inventory() {
+    document.getElementById("inventory").style.background = "#4b56f3";
+    document.getElementById("inventory").style.color = "white";
+}
+
+function inventory1() {
+    document.getElementById("inventory").style.background = "#fff";
+    document.getElementById("inventory").style.color = "black";
+}
+
+function report() {
+    document.getElementById("report").style.background = "#4b56f3";
+    document.getElementById("report").style.color = "white";
+}
+
+function report1() {
+    document.getElementById("report").style.background = "#fff";
+    document.getElementById("report").style.color = "black";
+}
+
+function saleinvoice() {
+    document.getElementById("saleinvoice").style.background = "#4b56f3";
+    document.getElementById("saleinvoice").style.color = "white";
+}
+
+function saleinvoice1() {
+    document.getElementById("saleinvoice").style.background = "#fff";
+    document.getElementById("saleinvoice").style.color = "black";
+}
+
+
+function printpage() {
+    var body = document.getElementById("body").innerHTML;
+    var data = document.getElementById("data").innerHTML;
+    document.getElementById("body").innerHTML = data;
+    window.print();
+
+}
+
+
